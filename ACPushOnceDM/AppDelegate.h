f512169ba4fadc09644e87f5a12655e1ff6f7998
albertchu <albertzyk@gmail.com>
2017-04-19 16:16:02 +0800

    Initial Commit

---
+++ ACPushOnceDM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ACPushOnceDM
+//
+//  Created by albertchu on 2017/4/19.
+//  Copyright © 2017年 albertchu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ACPushOnceDM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ACPushOnceDM
+//
+//  Created by albertchu on 2017/4/19.
+//  Copyright © 2017年 albertchu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
